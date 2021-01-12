9a3a18dff3afdd1970edd0c80f7a08c55e4b9e00
REDA <REDA@DESKTOP-SCF5LKQ>
2021-01-12 09:26:42 +0100

    test

---
+++ Freelance/Pages/Data/Repository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Freelance.Pages.Data
+{
+    public class Repository<T>
+    {
+    }
+}
